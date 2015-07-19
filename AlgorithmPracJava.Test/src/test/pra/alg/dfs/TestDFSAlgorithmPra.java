b01db28dd9267054b45a450b25486c84c19d72fa
ZeroTseng <returntozero0@gmail.com>
2015-07-19 22:43:01 +0800

    Init Algorithm Practice Project

---
+++ AlgorithmPracJava.Test/src/test/pra/alg/dfs/TestDFSAlgorithmPra.java
@@ -0,0 +1,17 @@
+package test.pra.alg.dfs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pra.alg.dfs.DFSAlgorithmPra;
+
+public class TestDFSAlgorithmPra {
+
+	@Test
+	public void test() {
+		DFSAlgorithmPra dfs = new DFSAlgorithmPra();
+		dfs.solve();
+	}
+
+}
